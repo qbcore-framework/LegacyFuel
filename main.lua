818a05b6b22b987df972b2a799b5d972c9e2ee20
vanillaa <80897991+evanillaa@users.noreply.github.com>
2022-01-02 16:18:02 +0200

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,10 @@
+local QBCore = exports['qb-core']:GetCoreObject()
+
+RegisterNetEvent('fuel:server:pay', function(price, source)
+	local Player = QBCore.Functions.GetPlayer(source)
+	local amount = math.floor(price + 0.5)
+
+	if price > 0 then
+		Player.Functions.RemoveMoney('cash', amount)
+	end
+end)
